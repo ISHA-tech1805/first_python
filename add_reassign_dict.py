ed1f1f95bb04cc3b29b4cf9a2d316ae7bb1ec561
Isha Singh <erishas0808@gmail.com>
2026-09-23 10:49:13 +0530

    added and updated in dictionary

---
+++ add_reassign_dict.py
@@ -0,0 +1,9 @@
+def add_entry(dic):
+    dic["number"]=8765343
+dic={"name": "nitin", "id": 32}   
+add_entry(dic)
+print("appended dictionary is:",dic) 
+def reassign(dic):
+    dic["name"]="isha"
+    print("updated dictionary is:", dic)
+reassign(dic)    
