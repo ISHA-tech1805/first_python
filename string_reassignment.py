d7b34830d97fb71c0a57c95c41aef94571071cec
Isha Singh <erishas0808@gmail.com>
2026-09-23 10:23:49 +0530

    7th program

---
+++ string_reassignment.py
@@ -0,0 +1,8 @@
+def change_string(str):
+    str1=str[1:]
+    str2="X"+ str1
+    return str2
+my_str="Hello"
+print("before reassignment :", my_str)
+reassign =change_string(my_str)
+print("after reassignmnet:", reassign)    
